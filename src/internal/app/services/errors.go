37a5e23cc6d0159ffc717878a082735858c6aef7
Anton Bilous <oxidate@tuta.io>
2025-11-19 13:55:17 +0200

    chore(errors): sort the errors and conversions

--- src/internal/app/services/errors.go
+++ src/internal/app/services/errors.go
@@ -2,26 +2,18 @@ package services
 
 import "errors"
 
-var (
-	ErrUnauthenticated = errors.New("unauthenticated, please log in (again)")
-
+// WARN(noatu): dot not add ErrForbidden, use *NotFound
+var ( // Remember to sort that!
+	ErrDatabaseError      = errors.New("database error")
+	ErrFileSystemError    = errors.New("file system error")
 	ErrForeignKeyViolated = errors.New("resource is still in use")
-	ErrTagAlreadyExists   = errors.New("tag already exists")
-
-	ErrItemTypeNotFound = errors.New("item type not found")
-	ErrItemNotFound     = errors.New("item not found")
-	ErrTagNotFound      = errors.New("tag not found")
-	ErrPictureNotFound  = errors.New("picture not found")
-
-	ErrInvalidImageFormat = errors.New("invalid image format")
 	ErrImageTooLarge      = errors.New("image is too large")
-
-	ErrNotImplemented = errors.New("method is not yet implemented")
-
-	// NOTE(noatu): errors below are internal details
-	ErrFileSystemError = errors.New("file system error")
-	ErrDatabaseError   = errors.New("database error")
-
-	// WARN(noatu): claims that something exists, use *NotFound
-	// ErrForbidden     = errors.New("forbidden")
+	ErrInvalidImageFormat = errors.New("invalid image format")
+	ErrItemNotFound       = errors.New("item not found")
+	ErrItemTypeNotFound   = errors.New("item type not found")
+	ErrNotImplemented     = errors.New("method is not yet implemented")
+	ErrPictureNotFound    = errors.New("picture not found")
+	ErrTagAlreadyExists   = errors.New("tag already exists")
+	ErrTagNotFound        = errors.New("tag not found")
+	ErrUnauthenticated    = errors.New("unauthenticated, please log in (again)")
 )
